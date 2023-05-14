bf171477ec5122990a81f53ef3588533052c0267
bfiskers <57647811+bfiskers@users.noreply.github.com>
2023-05-13 19:53:15 -0500

    First Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      hi: 1,
+      blue: "hello"
+    }
+  },
+  watch: {
+    blue() {
+      console.log(this.blue)
+    }
+  }
+}
+</script>
+
+<template>
+  <input v-model="blue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+  <div>
+    hi
+  </div>
+</template>
+
+<style scoped>
+</style>
